2f70e397b24065c5d5a7994b2b7741269e389a51
Sebatian Luis Enrique Aguilar Diaz <sebastian.luis.aguilar.diaz@gmail.com>
2025-11-30 15:19:55 -0600

    Funtion configs created

---
+++ farm.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cmath>
+#include<random>
+#include<time.h>
+#include<chrono>
+#include<string.h>
+#include<set>
+
+using namespace std;
+
+// Title:     			  
+// Author(s):  			  Sebastian Aguilar
+// Creation Date:		  11/30/25
+// Last update:			  11/30/25
+// Version:				  1.0.0
+
+
+int configs(){
+    
+    
+    
+    return 0;
+
+}
+
+
+int main(){
+    
+
+
+
+    return 0;
+}
